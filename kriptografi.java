45e1b64e40f96bdb02df7b0939010fc359864e71
destiaina <50942052+destiaina@users.noreply.github.com>
2020-11-03 07:27:08 +0700

    Add files via upload

---
+++ kriptografi.java
@@ -0,0 +1,8 @@
+package InaDestiaYuliana;
+
+public class kriptografi {
+    public static void main(String[] args) {
+        kriptografForm n = new kriptografForm();
+        n.setVisible(true);
+    }
+}
